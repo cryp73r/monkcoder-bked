21fdb638b400d25586d23df9694519650a9ae759
Priyanshu Singh <priyanshusingh0082@gmail.com>
2022-04-02 02:25:36 +0530

    basic user handling

---
+++ src/models/user.js
@@ -0,0 +1,78 @@
+const mongoose=require('mongoose')
+const validator=require('validator')
+const jwt=require('jsonwebtoken')
+const bcrypt=require('bcryptjs')
+
+const userSchema=mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('invalid email')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
+    timestamps: true
+})
+
+userSchema.methods.generateAuthToken=async function() {
+    const user=this
+    const token=jwt.sign({_id: user._id.toString()}, process.env.JWT_SECRET)
+    user.tokens=user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    delete userObject.password
+    delete userObject.tokens
+    return userObject
+}
+
+userSchema.statics.findByCredentials=async (email, password)=>{
+    const user=await User.findOne({email})
+    if (!user) {
+        throw new Error('Invalid credentials')
+    }
+    const isMatch=await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+        throw new Error('Invalid credentials')
+    }
+    return user
+}
+
+userSchema.pre('save', async function (next) {
+    const user=this
+    if (user.isModified('password')) {
+        const fname=user.name.split(" ")[0]
+        user.password=await bcrypt.hash(user.password, fname.length>8?fname.length:8)
+    }
+    next()
+})
+
+const User=mongoose.model('User', userSchema)
+
+module.exports=User
---
+++ src/routers/user.js
@@ -0,0 +1,156 @@
+const express=require('express')
+const User=require('../models/user')
+const auth=require('../middleware/auth')
+
+const router = express.Router()
+
+router.post('/user/create', async (req, res)=>{
+    const user=User(req.body)
+    try {
+        await user.save()
+        const token=await user.generateAuthToken()
+        res.status(201).send({
+            response: {
+                code: 201,
+                message: 'created'
+            },
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(400).send({
+            response: {
+                code: 400,
+                message: 'bad request'
+            }
+        })
+    }
+})
+
+router.post('/user', async (req, res)=>{
+    const email=req.body.email
+    const password=req.body.password
+    try {
+        const user=await User.findByCredentials(email, password)
+        const token=await user.generateAuthToken()
+        res.status(200).send({
+            response: {
+                code: 200,
+                message: 'logged-in'
+            },
+            user,
+            token
+        })
+    } catch (error) {
+        res.status(400).send({
+            response: {
+                codde: 400,
+                message: 'bad request'
+            }
+        })
+    }
+})
+
+router.get('/user', auth, (req, res)=>{
+    const user=req.user
+    const token=req.token
+    res.status(200).send({
+        response: {
+            code: 200,
+            message: 'successful'
+        },
+        user,
+        token
+    })
+})
+
+router.post('/user/logout', auth, async (req, res)=>{
+    const user=req.user
+    try {
+        user.tokens=user.tokens.filter((token)=>{
+            return token.token!==req.token
+        })
+        await user.save()
+        res.status(200).send({
+            response: {
+                code: 200,
+                message: 'logged-out'
+            },
+        })
+    } catch (error) {
+        res.status(500).send({
+            response: 500,
+            message: 'internal server error'
+        })
+    }
+})
+
+router.post('/user/logoutall', auth, async (req, res)=>{
+    const user=req.user
+    try {
+        user.tokens=[]
+        await user.save()
+        res.status(200).send({
+            response: 200,
+            message: 'logged-out-all'
+        })
+    } catch (error) {
+        res.status(500).send({
+            response: 500,
+            message: 'internal server error'
+        })
+    }
+})
+
+router.patch('/user', auth, async (req, res)=>{
+    const updates=Object.keys(req.body)
+    const allowedUpdates=['name', 'email', 'password']
+    const isValidOperation=updates.every((update)=>allowedUpdates.includes(update))
+    if (!isValidOperation) {
+        return res.status(400).send({
+            response: {
+                code: 400,
+                message: 'bad request'
+            }
+        })
+    }
+    const user=req.user
+    try {
+        updates.forEach((update)=>user[update]=req.body[update])
+        await user.save()
+        res.status(200).send({
+            response: {
+                code: 200,
+                message: 'updated'
+            },
+            user
+        })
+    } catch (error) {
+        res.status(400).send({
+            response: {
+                code: 400,
+                message: 'bad request'
+            }
+        })
+    }
+})
+
+router.delete('/user', auth, async (req, res)=>{
+    const user=req.user
+    try {
+        await user.remove()
+        res.status(200).send({
+            response: {
+                code: 200,
+                message: 'user deleted'
+            }
+        })
+    } catch (error) {
+        res.status(500).send({
+            response: 500,
+            message: 'internal server error'
+        })
+    }
+})
+
+module.exports = router
