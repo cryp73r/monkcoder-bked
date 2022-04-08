34f6c333a0415d4417be5e2b2b4aeaca293e8d5d
Priyanshu Singh <priyanshusingh0082@gmail.com>
2022-04-08 21:35:44 +0530

    redirects to repo for /

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,10 @@ const auth=require('../middleware/auth')
 
 const router = express.Router()
 
+router.get('/', (req, res)=>{
+    res.redirect('https://github.com/cryp73r/monkcoder-bked')
+})
+
 router.post('/user/create', async (req, res)=>{
     const user=new User(req.body)
     try {
